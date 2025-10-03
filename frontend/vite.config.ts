96bf0def610587e1d0130733fb16355483056469
Lilit Yepremyan <lilith.yepremyan@gmail.com>
2025-10-03 13:55:56 +0400

    smartify front+back

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import react from "@vitejs/plugin-react"
 import * as path from "node:path"
 import { defineConfig } from "vitest/config"
-import packageJson from "./package.json" with { type: "json" }
+import packageJson from "../package.json" with { type: "json" }
 
 // https://vitejs.dev/config/
 export default defineConfig({
